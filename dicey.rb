36ab3ece90121aa961a5cb012054fa202af35092
Alexandr Bulancov <6594487+trinistr@users.noreply.github.com>
2024-04-22 21:16:53 +0300

    Improve documentation, introduce optparse, add .ruby-version

--- dicey.rb
+++ dicey.rb
@@ -13,7 +13,7 @@
 class AbstractDie
   attr_reader :sides_list, :sides_num, :enum
 
-  # @param sides_list [Enumerable]
+  # @param sides_list [Enumerable<Object>]
   def initialize(sides_list)
     @sides_list = sides_list.dup.freeze
     @sides_num = @sides_list.size
@@ -27,9 +27,20 @@ class AbstractDie
     end
   end
 
-  # Get next side of the die, wrapping from last to first.
+  # Get next side of the die, advancing internal enumerator state.
+  # Wraps from last to first side.
+  #
+  # @return [Object] next side
   def next
     @enum.next
+  end
+
+  # Advance internal enumerator state by a random number using {#next}.
+  #
+  # @return [Object] rolled side
+  def roll
+    rand(0...sides_num).times { next }
+    @enum.peek
   end
 
   def to_s
@@ -63,8 +74,9 @@ class RegularDie < AbstractDie
 end
 
 # Base frequencies calculator.
+# @abstract
 class FrequenciesCalculator
-  # @param dice [Enumerable<RegularDie>]
+  # @param dice [Enumerable<AbstractDie>]
   # @return [Hash{Integer => Integer}] frequencies of each sum
   # @raise [RuntimeError] if dice list is invalid for the calculator
   def call(dice)
@@ -101,6 +113,10 @@ end
 # Able to handle {AbstractDie} lists with numeric sides.
 class GenericFrequenciesCalculator < FrequenciesCalculator
   private
+
+  def validate(dice)
+    dice.all? { |die| die.sides_list.all? { _1.is_a?(Integer) } }
+  end
 
   def calculate(dice)
     combine_dice_enumerators(dice).map(&:sum).tally
@@ -166,8 +182,8 @@ class RegularFrequenciesCalculator < FrequenciesCalculator
   # @see https://en.wikipedia.org/wiki/Pascal%27s_triangle
   # @see https://en.wikipedia.org/wiki/Trinomial_triangle
   def multinomial_coefficients(dice, sides, throw_away_garbage: true)
-    # This builds a right triangle where each first element is a 1.
-    # Each element is a sum of m elements in the previous row with indices less or equal to its,
+    # This builds a triangular matrix where each first element is a 1.
+    # Each element is a sum of `m` elements in the previous row with indices less or equal to its,
     # with out-of-bounds indices corresponding to 0s.
     # Example for m=3:
     # 1
@@ -176,7 +192,7 @@ class RegularFrequenciesCalculator < FrequenciesCalculator
     # 1 3 6 7 6 3 1, etc.
     coefficients = [[1]]
     (1..dice).each do |row_index|
-      row = next_row_of_coefficients(row_index, sides, coefficients.last)
+      row = next_row_of_coefficients(row_index, sides - 1, coefficients.last)
       if throw_away_garbage
         coefficients[0] = row
       else
@@ -187,10 +203,10 @@ class RegularFrequenciesCalculator < FrequenciesCalculator
   end
 
   def next_row_of_coefficients(row_index, window_size, previous_row)
-    length = row_index * (window_size - 1) + 1
+    length = row_index * window_size + 1
     (0..length).map do |col_index|
       # Have to clamp to 0 to prevent accessing array from the end.
-      window_range = ((col_index - (window_size - 1)).clamp(0..)..col_index)
+      window_range = ((col_index - window_size).clamp(0..)..col_index)
       window_range.sum { |i| previous_row.fetch(i, 0) }
     end
   end
@@ -199,19 +215,13 @@ end
 return unless $PROGRAM_NAME == __FILE__
 
 require 'tempfile'
+require 'optparse'
 
-if ARGV.empty? || ARGV.any? { ['-h', '--help'].include?(_1) }
-  puts 'Usage:'
-  puts "  #{Process.argv0} <number of sides> [<number of sides> ...]"
-  exit
-end
-
-calculators = [RegularFrequenciesCalculator.new, GenericFrequenciesCalculator.new]
-
-if ARGV.include?('--test')
+# A simple testing faculty for dealing with diceyness.
+class TestRunner
   # These are manually calculated frequencies,
   # with test cases for pretty much all variations of what this program can handle.
-  test_data = [
+  TEST_DATA = [
     [[1], { 1 => 1 }],
     [[10], { 1 => 1, 2 => 1, 3 => 1, 4 => 1, 5 => 1, 6 => 1, 7 => 1, 8 => 1, 9 => 1, 10 => 1 }],
     [[2, 2], { 2 => 1, 3 => 2, 4 => 1 }],
@@ -227,25 +237,81 @@ if ARGV.include?('--test')
     [[[3, 3, 3, 3, 3, 5, 5, 5]], { 3 => 5, 5 => 3 }],
     [[[1, 4, 6], [1, 4, 6]], { 2 => 1, 5 => 2, 7 => 2, 8 => 1, 10 => 2, 12 => 1 }],
     [[[3, 4, 3], [1, 3, 2]], { 4 => 2, 5 => 3, 6 => 3, 7 => 1 }]
-  ]
-  test_data.each do |test|
-    dice = test.first.map { _1.is_a?(Integer) ? RegularDie.new(_1) : AbstractDie.new(_1) }
-    print "#{dice.join(';')}:\n"
-    calculators.each do |calculator|
-      print "  #{calculator.class}: "
-      if calculator.valid_for?(dice)
-        calculator.call(dice) == test.last ? puts('✔') : puts('✘ <- failure!')
-      else
-        puts '☂'
+  ].freeze
+
+  # Strings for displaying test results.
+  RESULT_TEXT = { pass: '✔', fail: '✘ <- failure!', skip: '☂' }.freeze
+
+  # Check all tests defined in {TEST_DATA}.
+  #
+  # @param calculators [Array<FrequenciesCalculator>]
+  # @param report_style [:full, :quiet]
+  # @return [Boolean] whether there are no failing tests
+  def call(calculators, report_style)
+    results = TEST_DATA.to_h { |test| run_test(test, calculators) }
+    full_report(results) if report_style == :full
+    results.values.none? { |test_result| test_result.values.any? { _1 == :fail } }
+  end
+
+  private
+
+  # @param test [Array(Array<Integer, Array<Integer>>, Hash{Integer => Integer})]
+  #   pair of a dice list definition and expected results
+  # @return [Array(Array<AbstractDie>, Hash{FrequenciesCalculator => :pass, :fail, :skip})]
+  #   result of running the test in a format suitable for +#to_h+
+  def run_test(test, calculators)
+    dice = build_dice(test.first)
+    test_result = calculators.each_with_object({}) do |calculator, hash|
+      hash[calculator] =
+        if calculator.valid_for?(dice)
+          calculator.call(dice) == test.last ? :pass : :fail
+        else
+          :skip
+        end
+    end
+    [dice, test_result]
+  end
+
+  # Build a list of {AbstractDie} objects from a plain definition.
+  #
+  # @param definition [Array<Integer, Array<Integer>>]
+  # @return [Array<AbstractDie>]
+  def build_dice(definition)
+    definition.map { _1.is_a?(Integer) ? RegularDie.new(_1) : AbstractDie.new(_1) }
+  end
+
+  # Print results of running all tests.
+  def full_report(results)
+    results.each do |dice, test_result|
+      print "#{dice.join(';')}:\n"
+      test_result.each do |calculator, result|
+        print "  #{calculator.class}: "
+        puts RESULT_TEXT[result]
       end
     end
   end
-  return
 end
+
+# List of calculators to use, ordered by efficiency.
+calculators = [RegularFrequenciesCalculator.new, GenericFrequenciesCalculator.new]
+
+option_parser = OptionParser.new do |parser|
+  parser.banner = "Usage: #{Process.argv0} <number of sides> [<number of sides> ...]"
+  parser.on_tail('-h', '--help', 'Show this help') do
+    puts parser
+    exit
+  end
+  parser.on('--test [REPORT_STYLE]', %w[full quiet], 'Validate that calculators give expected results.',
+            'REPORT_STYLE can be either `full` (default) or `quiet`.') do |report_style|
+    exit TestRunner.new.call(calculators, report_style&.to_sym || :full)
+  end
+end
+options = {}
+arguments = option_parser.parse!(into: options)
 
 # Actually run the calculations!
 
-dice = ARGV.map { RegularDie.new(_1.to_i) }
+dice = arguments.map { RegularDie.new(_1.to_i) }
 frequencies = calculators.find { _1.valid_for?(dice) }.call(dice)
 
 sides_list = dice.map(&:sides_num).join(',')
