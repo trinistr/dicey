6357085828997a07d0590d6b92d20cd50af6273b
Alexandr Bulancov <6594487+trinistr@users.noreply.github.com>
2025-09-30 21:23:43 +0300

    maybe like this?

--- spec/dicey/distribution_properties_calculator_spec.rb
+++ spec/dicey/distribution_properties_calculator_spec.rb
@@ -80,21 +80,20 @@ module Dicey
       end
     end
 
-    context "when distribution is complex" do
-      let(:distribution) { { 1i => 2, 2 => 3, Complex(2, 3) => 2, Complex(1, 4) => 3 } }
+    if RUBY_ENGINE == "ruby"
+      # Hangs on JRuby, Complex+Rational raises on TruffleRuby
+      context "when distribution is complex" do
+        let(:distribution) { { 1i => 2, 2 => 3, Complex(2, 3) => 2, Complex(1, 4) => 3 } }
 
-      before do
-        pending "Hangs on JRuby, Complex+Rational raises on TruffleRuby" if RUBY_ENGINE != "ruby"
-      end
-
-      it "returns all properties which don't depend on ordering" do
-        expect(properties).to include(
-          mode: [2, 1 + 4i],
-          arithmetic_mean: (5/4r) + 2i,
-          expected_value: (13/10r) + 2i,
-          variance: (-219/100r) - (2/5r).i
-        )
-        expect(properties).to include(:standard_deviation, :skewness, :kurtosis, :excess_kurtosis)
+        it "returns all properties which don't depend on ordering" do
+          expect(properties).to include(
+            mode: [2, 1 + 4i],
+            arithmetic_mean: (5/4r) + 2i,
+            expected_value: (13/10r) + 2i,
+            variance: (-219/100r) - (2/5r).i
+          )
+          expect(properties).to include(:standard_deviation, :skewness, :kurtosis, :excess_kurtosis)
+        end
       end
     end
 
