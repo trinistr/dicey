0434b34003ab4f891e471dbd8a61208420902262
Alexandr Bulancov <6594487+trinistr@users.noreply.github.com>
2025-09-30 20:42:11 +0300

    Add a test for complex distribution

--- spec/dicey/distribution_properties_calculator_spec.rb
+++ spec/dicey/distribution_properties_calculator_spec.rb
@@ -80,6 +80,20 @@ module Dicey
       end
     end
 
+    context "when distribution is complex" do
+      let(:distribution) { { 1i => 2, 2 => 3, Complex(2, 3) => 2, Complex(1, 4) => 3 } }
+
+      it "returns all properties which don't depend on ordering" do
+        expect(properties).to include(
+          mode: [2, 1 + 4i],
+          arithmetic_mean: (5/4r) + 2i,
+          expected_value: (13/10r) + 2i,
+          variance: (-219/100r) - (2/5r).i
+        )
+        expect(properties).to include(:standard_deviation, :skewness, :kurtosis, :excess_kurtosis)
+      end
+    end
+
     context "when distribution includes non-numeric values" do
       let(:distribution) { { "a" => 3, 2 => 2, "c" => 2, 4 => 3, "e" => 2, 6 => 3 } }
 
