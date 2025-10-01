8c70e87153ca16c1a50203aa87e63e4ca7e52d72
Alexander Bulancov <6594487+trinistr@users.noreply.github.com>
2025-10-01 11:49:09 +0300

    Add `DistributionPropertiesCalculator` (#20)
    * Add `DistributionPropertiesCalculator`
    
    * Speed up spec for `Empirical` calculator (at the cost of precision)
    
    * Add tests, protect against interesting cases

---
+++ lib/dicey/distribution_properties_calculator.rb
@@ -0,0 +1,122 @@
+# frozen_string_literal: true
+
+require_relative "rational_to_integer"
+
+module Dicey
+  # Calculates distribution properties,
+  # also known as descriptive statistics when applied to a population sample.
+  #
+  # These are well-known properties such as:
+  # - min, max, mid-range;
+  # - mode, median, arithmetic mean;
+  # - important moments (expected value, variance, skewness, kurtosis).
+  #
+  # It is notable that most dice create symmetric distributions,
+  # which means that skewness is 0, while properties denoting center in some way
+  # (median, mean, ...) are all equal.
+  # Mode is often not unique, but includes this center.
+  class DistributionPropertiesCalculator
+    include RationalToInteger
+
+    # Calculate properties for a given distribution.
+    #
+    # Depending on values in the distribution, some properties may be undefined.
+    # In such cases, only mode is guaranteed to be present.
+    #
+    # @param distribution [Hash{Numeric => Numeric}
+    #   numeric distribution with pre-sorted keys
+    # @return [Hash{Symbol => Numeric, Array<Numeric>}]
+    def call(distribution)
+      return {} if distribution.empty?
+
+      calculate_properties(distribution)
+    end
+
+    private
+
+    def calculate_properties(distribution)
+      outcomes = distribution.keys
+      weights = distribution.values
+
+      {
+        mode: mode(outcomes, weights),
+        **range_characteristics(outcomes),
+        **median(outcomes),
+        **means(outcomes, weights),
+        **moments(distribution),
+      }
+    end
+
+    def mode(outcomes, weights)
+      max_weight = weights.max
+      outcomes.select.with_index { |_, index| weights[index] == max_weight }
+    end
+
+    def range_characteristics(outcomes)
+      min = outcomes.min
+      max = outcomes.max
+      {
+        min: min,
+        max: max,
+        total_range: max - min,
+        mid_range: rational_to_integer(Rational(min + max, 2)),
+      }
+    rescue ArgumentError, TypeError, NoMethodError
+      # Outcomes are not comparable with each other, so a range can not be determined.
+      {}
+    end
+
+    def means(outcomes, _weights)
+      {
+        arithmetic_mean: rational_to_integer(Rational(outcomes.sum, outcomes.size)),
+      }
+    rescue ArgumentError, TypeError
+      # Outcomes are not summable with each other, means are meaningless.
+      {}
+    end
+
+    def median(outcomes)
+      outcomes = outcomes.sort
+      value =
+        if outcomes.size.odd?
+          outcomes[outcomes.size / 2]
+        else
+          Rational(outcomes[(outcomes.size / 2) - 1] + outcomes[outcomes.size / 2], 2)
+        end
+      { median: value }
+    rescue ArgumentError, TypeError, NoMethodError
+      # Outcomes are not compatible with each other, so a median can not be determined.
+      {}
+    end
+
+    def moments(distribution)
+      total_weight = distribution.values.sum
+      expected_value = rational_to_integer(moment(distribution, total_weight, 1))
+      variance = rational_to_integer(moment(distribution, total_weight, 2) - (expected_value**2))
+      skewness =
+        rational_to_integer(moment(distribution, total_weight, 3, expected_value, variance))
+      kurtosis =
+        rational_to_integer(moment(distribution, total_weight, 4, expected_value, variance))
+
+      {
+        expected_value: expected_value,
+        variance: variance,
+        standard_deviation: Math.sqrt(variance),
+        skewness: skewness,
+        kurtosis: kurtosis,
+        excess_kurtosis: kurtosis ? kurtosis - 3 : nil,
+      }
+    rescue ArgumentError, TypeError, NoMethodError
+      # Outcomes are not compatible with each other, moments are fleeing.
+      {}
+    end
+
+    def moment(distribution, total_weight, degree, center = 0, variance = nil)
+      # With 0 variance, normalized moments are undefined.
+      return nil if variance == 0 # rubocop:disable Style/NumericPredicate
+
+      unnormalized = distribution.sum { |r, w| ((r - center)**degree) * Rational(w, total_weight) }
+      variance ? (unnormalized / (variance**(degree/2r))) : unnormalized
+    end
+  end
+end
---
+++ spec/dicey/distribution_properties_calculator_spec.rb
@@ -0,0 +1,116 @@
+# frozen_string_literal: true
+
+module Dicey
+  RSpec.describe DistributionPropertiesCalculator do
+    subject(:properties) { calculator.call(distribution) }
+
+    let(:calculator) { described_class.new }
+
+    context "when distribution is empty" do
+      let(:distribution) { {} }
+
+      it "returns an empty hash" do
+        expect(properties).to eq({})
+      end
+    end
+
+    context "when distribution has only a single real-valued point" do
+      let(:distribution) { { 37 => 1 } }
+
+      it "returns properties with trivial values, some of them undefined" do
+        expect(properties).to eq(
+          mode: [37],
+          min: 37,
+          max: 37,
+          mid_range: 37,
+          total_range: 0,
+          median: 37,
+          arithmetic_mean: 37,
+          expected_value: 37,
+          variance: 0,
+          standard_deviation: 0,
+          skewness: nil,
+          kurtosis: nil,
+          excess_kurtosis: nil
+        )
+      end
+    end
+
+    context "when distribution is uniform" do
+      let(:distribution) { { 1 => 1, 2 => 1, 3 => 1, 4 => 1, 5 => 1, 6 => 1 } }
+
+      it "returns expected values for properties" do
+        expect(properties).to eq(
+          mode: [1, 2, 3, 4, 5, 6],
+          min: 1,
+          max: 6,
+          mid_range: (7/2r),
+          total_range: 5,
+          median: (7/2r),
+          arithmetic_mean: (7/2r),
+          expected_value: (7/2r),
+          variance: (35/12r),
+          standard_deviation: Math.sqrt(35/12r),
+          skewness: 0,
+          kurtosis: (303/175r),
+          excess_kurtosis: (-222/175r)
+        )
+      end
+    end
+
+    context "when distribution is asymmetric" do
+      let(:distribution) { { 1 => 1, 2 => 3, 3 => 2, 4 => 5, 5 => 1, 60 => 2 } }
+
+      it "returns expected values for properties" do
+        expect(properties).to match(
+          mode: [4],
+          min: 1,
+          max: 60,
+          mid_range: (61/2r),
+          total_range: 59,
+          median: (7/2r),
+          arithmetic_mean: (75/6r),
+          expected_value: (158/14r),
+          variance: a_value_within(0.00000001).of(396.63265306),
+          standard_deviation: a_value_within(0.00000001).of(Math.sqrt(396.63265306)),
+          skewness: a_value_within(0.00000001).of(2.02910507),
+          kurtosis: a_value_within(0.00000001).of(5.14047263),
+          excess_kurtosis: a_value_within(0.00000001).of(2.14047263)
+        )
+      end
+    end
+
+    if RUBY_ENGINE == "ruby"
+      # Hangs on JRuby, Complex+Rational raises on TruffleRuby
+      context "when distribution is complex" do
+        let(:distribution) { { 1i => 2, 2 => 3, Complex(2, 3) => 2, Complex(1, 4) => 3 } }
+
+        it "returns all properties which don't depend on ordering" do
+          expect(properties).to include(
+            mode: [2, 1 + 4i],
+            arithmetic_mean: (5/4r) + 2i,
+            expected_value: (13/10r) + 2i,
+            variance: (-219/100r) - (2/5r).i
+          )
+          expect(properties).to include(:standard_deviation, :skewness, :kurtosis, :excess_kurtosis)
+        end
+      end
+    end
+
+    context "when distribution includes non-numeric values" do
+      let(:distribution) { { "a" => 3, 2 => 2, "c" => 2, 4 => 3, "e" => 2, 6 => 3 } }
+
+      it "returns only mode" do
+        expect(properties).to eq(mode: ["a", 4, 6])
+      end
+    end
+
+    context "when distribution includes non-sortable values" do
+      let(:distribution) { { "a" => 1, {} => 2, 3 => 3 } }
+
+      it "returns only mode" do
+        expect(properties).to eq(mode: [3])
+      end
+    end
+  end
+end
