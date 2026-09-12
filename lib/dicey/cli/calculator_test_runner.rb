82f55ab522f427473e12cc1202be881fe089b973
Alexander Bulancov <6594487+trinistr@users.noreply.github.com>
2026-09-13 00:42:03 +0300

    Fix SimpleCov error on older Rubies
    d500ca383233dc192206cc040431f1f34a4b8c2f made CI fail because of course it would.
    
    - Rescue error that happens when SimpleCov can't deal with new API.
    - Remove default filter that skipped tests. I do want to check coverage there.
    - Use line-tail forms of `simplecov:disable`.

--- lib/dicey/cli/calculator_test_runner.rb
+++ lib/dicey/cli/calculator_test_runner.rb
@@ -46,9 +46,7 @@ module Dicey
            Complex(1, 2) => 1, Complex(2, 2) => 1, Complex(3, 2) => 1,
            Complex(1, 3) => 1, Complex(2, 3) => 1, Complex(3, 3) => 1 }],
         *(
-          # simplecov:disable
-          if defined?(VectorNumber)
-            # simplecov:enable
+          if defined?(VectorNumber) # simplecov:disable
             [
               [[["s", "a", "d", 33]],
                { VectorNumber["s"] => 1, VectorNumber["a"] => 1, VectorNumber["d"] => 1, 33 => 1 }],
--- spec/support/coverage_helper.rb
+++ spec/support/coverage_helper.rb
@@ -14,7 +14,12 @@ SimpleCov.start do
 
   group "Lib", "lib"
   group "Tests", "spec"
+  remove_filter %r{\A(test|features|spec|autotest)/}
 
   SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
   SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter, SimpleCov::Formatter::LcovFormatter]
+rescue RuntimeError => e
+  raise unless e.message.start_with?("Unsupported coverage criterion eval")
+
+  warn "simplecov is too old, coverage report will not be generated!"
 end
